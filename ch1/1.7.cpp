3eb6bfbc1cc27a014a5bb3f00c527ec130a7b910
oiu850714 <oiu850714@gmail.com>
2018-01-09 13:26:44 +0800

    and exercise solutions in section 1.3

---
+++ ch1/1.7.cpp
@@ -0,0 +1,6 @@
+int main()
+{
+    /* wrong nested /*
+    comment */ */
+    return 0;
+}
---
+++ ch1/1.8.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main()
+{
+    std::cout << "/*"; // legal
+    std::cout << "*/"; // legal
+    std::cout << /* "*/" */; // illegal";
+    std::cout << /* "*/" /* "/*" */; // legal
+}

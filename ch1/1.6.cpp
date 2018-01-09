3d50850a9774728c6450ebc0fa7a646dd4d322e0
oiu850714 <oiu850714@gmail.com>
2018-01-09 09:39:45 +0800

    and exercise solutions in section 1.2

---
+++ ch1/1.6.cpp
@@ -0,0 +1,9 @@
+//no code
+/*
+    The following program fragment is illegal:
+    std::cout << "The sum of " << v1; << " and " << v2;
+              << " is " << v1 + v2 << std::endl;
+    Fix:
+    std::cout << "The sum of " << v1 << " and " << v2
+              << " is " << v1 + v2 << std::endl;
+*/

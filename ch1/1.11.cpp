ea20276e23a136ba0ec5bde8992ab0f63da86e8e
oiu850714 <oiu850714@gmail.com>
2018-01-09 13:32:27 +0800

    and exercise solutions in section 1.4.1

---
+++ ch1/1.11.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Enter two numbers, I'll print numbers between these two numbers:";
+    int i1 = 0;
+    int i2 = 0;
+    std::cin >> i1 >> i2;
+    while(i1 <= i2)
+    {
+        std::cout << i1++ << std::endl;
+    }
+    return 0;
+}
---
+++ ch1/1.9.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    int i = 50;
+    int result = 0;
+    while(i <= 100)
+    {
+        result += i;
+        ++i;
+    }
+    std::cout << "sum(50 to 100): " << result << std::endl;
+    return 0;
+}

177501f510578892cecf7b316b040e8b69d60e68
oiu850714 <oiu850714@gmail.com>
2018-01-09 15:53:56 +0800

    add exercise solutions in section 1.5.2

---
+++ ch1/1.23.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "include/Sales_item.h"
+
+int main()
+{
+    Sales_item cur_item, new_item;
+    int has_input_one_item_flag = 0;
+    int num_transactions = 0;
+    while(std::cin >> new_item)
+    {
+        if(cur_item.isbn() == new_item.isbn())
+        {
+            cur_item += new_item;
+            num_transactions++;
+        }
+        else
+        {
+            if(has_input_one_item_flag)
+            {
+                std::cout << "number of transactions: " << num_transactions << std::endl;
+                std::cout << cur_item << std::endl;
+            }
+            else
+            {
+                has_input_one_item_flag = 1;
+                num_transactions = 1;
+            }
+            num_transactions = 1;
+            cur_item = new_item;
+        }
+    }
+    std::cout << "number of transactions: " << num_transactions << std::endl;
+    std::cout << cur_item << std::endl;
+
+    return 0;
+}

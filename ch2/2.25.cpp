86af9aaaaac9c82d540012980538205a34570229
oiu850714 <oiu850714@gmail.com>
2018-01-11 18:18:27 +0800

    add exercise solutions in section 2.3.3

---
+++ ch2/2.25.cpp
@@ -0,0 +1,8 @@
+int main() {
+  //(a)
+  int *ip1, i1, &r = i1; // ip1: pointer to int, i1: int, r: reference to int
+  //(b)
+  int i2, *ip2 = 0; // i2: int, ip2: pointer to int
+  //(c)
+  int *ip3, ip4; // ip3: pointer to int, ip4: int
+}

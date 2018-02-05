d92d38447496646c012a929c9698a7e39ef03d82
oiu850714 <oiu850714@gmail.com>
2018-02-05 23:23:46 +0800

    update 7.41.cpp, define first_item in main

--- ch7/7.41.cpp
+++ ch7/7.41.cpp
@@ -61,9 +61,9 @@ std::istream &read(std::istream &is, Sales_data &data) {
 
 using std::cin;
 
-Sales_data first_item(cin); // Sales_data(std::istream &is);
 int main() {
-  Sales_data next; // Sales_data(const std::string &s = "");
+  Sales_data first_item(cin); // Sales_data(std::istream &is);
+  Sales_data next;            // Sales_data(const std::string &s = "");
   // bookNo = "", units_sold = 0, revenue = 0;
   Sales_data last("9-999-99999-9"); // Sales_data(const std::string &s = "");
   // bookNo = "9-999-99999-9", units_sold = 0, revenue = 0;

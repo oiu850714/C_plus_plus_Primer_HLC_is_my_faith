46de134f572582b9f9b45c2cbf65f731423079e4
oiu850714 <oiu850714@gmail.com>
2020-04-27 20:00:43 +0800

    add test.cpp

---
+++ ch16/test.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <iostream>
+#include <memory>
+#include <algorithm>
+#include <string>
+
+#include <src/Vec.h>
+#include <src/Blob.h>
+
+#include <src/BlobPtr.h>
+#include <src/some_utils.h>
+
+int main() {
+    Vec<int> V1{1, 2, 3, 4, 5};
+}

72fb9241ab2e154b942ff674727a25aebfea2ddd
oiu850714 <oiu850714@gmail.com>
2020-06-28 15:29:19 +0800

    add solution for 18.21

---
+++ ch18/18.21.cpp
@@ -0,0 +1,3 @@
+// (a) coorect. CADVehicle public inherits CAD, private inherits Vehicle
+// (b) error. Cannot direct inherit same class more than once
+// (c) correct. iostream: public inherits istream, public inherits ostream
